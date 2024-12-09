bbe02a714a9b366ee3034630ce2c073ceb331b25
Ekaterina Bezheskaia <bezheskaya@gmail.com>
2024-12-09 23:41:20 +0400

    DTO

--- src/main/java/com/example/testtask/api/dto/HandbookAddressesDTO.java
+++ src/main/java/com/example/testtask/api/dto/HandbookAddressesDTO.java
@@ -1,4 +1,33 @@
 package com.example.testtask.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Data
+@Builder(builderMethodName = "fileDtoBuilder")
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
 public class HandbookAddressesDTO {
+
+    @NonNull
+    Long id;
+
+    @NonNull
+    String titleAddress;
+
+    @NonNull
+    String literal;
+
+    @NonNull
+    @JsonProperty("creation_date")
+    Instant creationDate = Instant.now();
+
+    @NonNull
+    String description;
+
 }
