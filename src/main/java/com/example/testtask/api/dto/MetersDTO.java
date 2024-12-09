e4e9ba197ac63e81c48bb17fba0607458bdc4cdf
Ekaterina Bezheskaia <bezheskaya@gmail.com>
2024-12-09 23:42:18 +0400

    DTO

--- src/main/java/com/example/testtask/api/dto/MetersDTO.java
+++ src/main/java/com/example/testtask/api/dto/MetersDTO.java
@@ -1,4 +1,30 @@
 package com.example.testtask.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Data
+@Builder(builderMethodName = "fileDtoBuilder")
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
 public class MetersDTO {
+
+    @NonNull
+    Long id;
+
+    @NonNull
+    String titleMetersNumber;
+
+    @NonNull
+    @JsonProperty
+    Instant startTime;
+
+    @NonNull
+    String description;
+
 }
